4c9db3e8d819c74248feb77924edefd61bcb0347
Nurik <you@example.com>
2022-11-26 03:26:20 +0300

    Double numbers

---
+++ Double_Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿double[,] array = GetArray(3,4);
+PrintArray(array);
+
+double [,] GetArray(int row, int col)
+{
+    double[,] array = new double[row,col];
+    var rnd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            array[i,j] = rnd.NextDouble();
+        }
+    }
+
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
