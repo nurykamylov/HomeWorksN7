eae62048e734e5e1168c0d41042826f3f92999bb
Nurik <you@example.com>
2022-12-10 04:14:57 +0300

    Pohuy

---
+++ Average/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+Задача 52. Задайте двумерный массив из целых чисел. 
+Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
+*/
+
+int[,] array = GetArray(3, 4);
+PrintArray(array);
+System.Console.WriteLine();
+AverageOfColumns(array);
+
+
+int [,] GetArray(int row, int col)
+{
+    int[,] array = new int[row,col];
+    var rnd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = rnd.Next(1,10);
+        }
+    }
+
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void AverageOfColumns(int[,] arr)
+{
+    
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            sum += arr[i, j];
+        }
+        double average = sum / arr.GetLength(0);
+       Console.Write(average + "\t");
+    }
+    
+}
